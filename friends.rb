639a96a647ca11bcd00cb6595c743b9dce80b8f1
Gauri Joshi <gauri728@gmail.com>
2022-04-20 13:32:31 -0700

    Update friends

--- friends.rb
+++ friends.rb
@@ -1,7 +1,10 @@
-best_friend = "Mike"
+best_friend = "Craig"
+another_friend = "Libby"
 
 if best_friend == "Mike"
   puts "Hey #{best_friend}, thanks for visiting"
+elsif another_friend == "Libby"
+  puts "Hey #{another_friend}, thanks for stopping by"
 else
   puts "Hey #{best_friend}, i still like you"
 end
